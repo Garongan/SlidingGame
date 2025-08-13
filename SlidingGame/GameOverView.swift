19a203c88f3d6f992d514c743f74c58386775335
Garongan <alvindo56@gmail.com>
2025-08-14 00:48:49 +0700

    feat: gameplay

---
+++ SlidingGame/GameOverView.swift
@@ -0,0 +1,18 @@
+//
+//  GameOverView.swift
+//  SlidingGame
+//
+//  Created by Alvindo Tri Jatmiko on 13/08/25.
+//
+
+import SwiftUI
+
+struct GameOverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameOverView()
+}
---
+++ SlidingGame/ViewRouter.swift
@@ -0,0 +1,8 @@
+//
+//  ViewRouter.swift
+//  SlidingGame
+//
+//  Created by Alvindo Tri Jatmiko on 13/08/25.
+//
+
+import Foundation
